625d6c201a0373fb6f78eeda0398b0ce03d83fac
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-05-07 21:24:27 +0800

    Move server codebase to 1 file

---
+++ server/index.js
@@ -0,0 +1,34 @@
+// Modules
+const express = require("express");
+const bodyparser = require("body-parser");
+const { EventEmitter } = require("events");
+
+// Constants
+const events = new EventEmitter();
+const ban = [];
+const port = 3000;
+
+// Express app
+const app = express();
+app.use(bodyparser.json());
+
+// Roblox server request bans from webserver
+// Waits until discord bot sends ban data
+app.get("/ban", (_req, res) =>
+	events.once("banEvent", bans => {
+		res.json(bans)
+		ban = [];
+	})
+);
+
+// Discord bot sends ban data to webserver
+app.post("/ban", (req, res) => {
+	ban.push(req.body);
+	events.emit("banEvent", ban);
+});
+
+// Send status 200 to client
+app.get("/", (_req, res) => res.sendStatus(200));
+
+// Start webserver
+app.listen(port, () => console.log(`Listening on port ${port}`));

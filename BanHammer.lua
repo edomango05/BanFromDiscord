3f32352cee9e3981386d69ff5e0b10d3a2830746
pollovolante <64164316+pollovolante@users.noreply.github.com>
2021-05-07 12:53:44 +0200

    Update BanHammer.lua

--- BanHammer.lua
+++ BanHammer.lua
@@ -10,12 +10,6 @@ local notificationWebhook = ""
 local endpoint = "http://ip:3000/ban"
 local playersToDestroy = {}
 function parser(time)
-	local timeObject = {
-		m= 60,
-		h= 3600,
-		g= 86400,
-		s=604800
-	}
 	if time >= 60 and time < 3600 then
 		return tostring(time/60).." minutes"
 	elseif time >= 3600 and time < 86400 then
